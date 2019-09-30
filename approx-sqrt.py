7f0082bbc00aca73972e26fead87dd12c768b45a
Morgan Reilly <morgan.reilly27@gmail.com>
2019-09-30 23:30:47 +0100

    Getting better approximation

--- approx-sqrt.py
+++ approx-sqrt.py
@@ -1,10 +1,12 @@
+# https://tour.goland/org/flowcontrol/8
+
 def sqrt(x):
     # Initial guess
 
     z = 1.0
     # Keep getting a better estimate for the square root
     # of x, until you are within two decimal places
-    while abs(z * z - x) >= 0.01:
+    while abs(z * z - x) >= 0.0001:
         # get better approximation for square root
         z -= (z*z -x) / (2*z)   
     return z
